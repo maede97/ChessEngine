8176ce90396d99e6633ec8289efb2348a6557a29
Matthias Busenhart <busenham@student.ethz.ch>
2021-02-16 09:35:44 +0100

    Add docs for piece

--- chessEngine/include/chessEngine/piece.h
+++ chessEngine/include/chessEngine/piece.h
@@ -4,25 +4,43 @@
 namespace chessEngine
 {
     /** 
- * A chess Piece
- */
+     * @brief Representation of a chess piece.
+     */
     class Piece
     {
     public:
+        /**
+         * @brief We do not allow the default constructor.
+         */
         Piece() = delete;
+        /**
+         * @brief Construct a piece from a type and a color.
+         * @param type The PieceType to be used.
+         * @param color The PlayerColor to be used.
+         */
         Piece(PieceType type, PlayerColor color);
 
-        // getters
+        /**
+         * @brief Getter for the type.
+         */
         PieceType type() const;
+        /**
+         * @brief Getter for the color.
+         */
         PlayerColor color() const;
 
-        // setters by reference
+        /**
+         * @brief Setter for the type.
+         */
         PieceType &type();
+        /**
+         * @brief Setter for the color.
+         */
         PlayerColor &color();
 
     private:
-        PieceType m_type;
-        PlayerColor m_color;
+        PieceType m_type; ///< Internal type
+        PlayerColor m_color; ///< Internal color
     };
 
 } // namespace chessEngine
