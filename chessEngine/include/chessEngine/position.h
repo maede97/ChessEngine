8f7faabc5353ed40f2bbf69e2d38cb78c41e0d5f
Matthias Busenhart <busenham@student.ethz.ch>
2021-02-16 09:39:51 +0100

    Add docs to position

--- chessEngine/include/chessEngine/position.h
+++ chessEngine/include/chessEngine/position.h
@@ -5,27 +5,52 @@
 namespace chessEngine
 {
     /**
- * Class which holds information about a position on the board.
- */
+     * @brief This class helds information about a single position on the Board.
+     */
     class Position
     {
 
     public:
+        /**
+         * @brief We do not allow the default constructor.
+         */
         Position() = delete;
-
+        /**
+         * @brief Constructs a position from a given row and column.
+         * @param row The row, starting from 0 (top) to 7 (bottom).
+         * @param column The column, starting from 0 (left) to 7 (right).
+         * @throws std::runtime_error if the indices are out of bounds.
+         */
         Position(uint8_t row, uint8_t col);
 
+        /**
+         * @brief Getter for the row
+         */
         uint8_t row() const;
+        /**
+         * @brief Getter for the column
+         */
         uint8_t col() const;
 
+        /**
+         * @brief Getter for row and column.
+         * @param row Where to write the row.
+         * @param column Where to write the column.
+         */
         void get(uint8_t &row, uint8_t &column) const;
 
-        // make the internal representation public, such that this can be used as a key for a hashmap
+        /**
+         * @brief A hash of the current position, which is unique for each position on the board.
+         * @return The hash value (8 bits suffice for 64 positions).
+         */
         uint8_t hash() const;
 
     private:
-        // 6 bits are enough to represent every position on the board
-        // however, in C++, the smallest type is 8 bits, therefore we use this
+        /**
+         * @brief The internal position store.
+         * 6 bits are enough to represent every position on the board
+         * however, in C++, the smallest type is 8 bits, therefore we use this
+         */
         uint8_t m_internal_position;
     };
 
