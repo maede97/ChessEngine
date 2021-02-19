7a5c17dfffb414d1be60e20b066652831cd07a1b
Matthias Busenhart <busenham@student.ethz.ch>
2021-02-19 23:14:04 +0100

    Add more comments

--- chessEngine/include/chessEngine/board.h
+++ chessEngine/include/chessEngine/board.h
@@ -126,6 +126,7 @@ public:
    * @param position The position of the piece to get the valid moves.
    * @throws std::runtime_error If no piece is in this position.
    * @attention This does not cover attack moves from pawns!
+   * @todo Filter out moves which are not possible because of other pieces in the way.
    */
   std::vector<std::vector<bool>> getValidMoves(const Position &position);
 
